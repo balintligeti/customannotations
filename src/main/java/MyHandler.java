526961db34152eeaa471b517af6491a7ec1e3daa
Balint Ligeti <balint.ligeti@hotmail.com>
2020-09-27 17:32:28 +0200

    Update Maven project's dependencies.

---
+++ src/main/java/MyHandler.java
@@ -0,0 +1,18 @@
+import java.io.IOException;
+import java.io.OutputStream;
+import java.net.InetSocketAddress;
+
+import com.sun.net.httpserver.HttpExchange;
+import com.sun.net.httpserver.HttpHandler;
+import com.sun.net.httpserver.HttpServer;
+
+public class MyHandler implements HttpHandler {
+        @Override
+        public void handle(HttpExchange t) throws IOException {
+                String response = "This! is the response";
+                t.sendResponseHeaders(200, response.length());
+                OutputStream os = t.getResponseBody();
+                os.write(response.getBytes());
+                os.close();
+        }
+}
